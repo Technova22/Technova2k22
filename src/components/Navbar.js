099db77352f9d10905248dfd7cb1f65784113f15
abhinavaa01 <agarwalabhinav91@gmail.com>
2022-11-27 00:46:00 +0530

    Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,66 +5,71 @@ import { Dropdown } from 'rsuite';
 import '../Navbar.css'
 import 'rsuite/dist/rsuite.min.css';
 const Navbar1 = () => {
-    const currentName = useStore((state) => state.currentName);
-const { setCurrentName ,setCurrentEmail} = useStore();
-const location = useLocation();
-console.log(location.pathname)
-const refer = useRef(null);
-window.addEventListener('scroll', fixNav)
-function fixNav() {
-    if(window.scrollY >  150) {
-        refer.current.classList.add('active1');
+  const currentName = useStore((state) => state.currentName);
+  const { setCurrentName, setCurrentEmail } = useStore();
+  const location = useLocation();
+  console.log(location.pathname)
+  const refer = useRef(null);
+  window.addEventListener('scroll', fixNav)
+  function fixNav() {
+    if (window.scrollY > 150) {
+      refer.current.classList.add('active1');
     } else {
-        refer.current.classList.remove('active1');
+      refer.current.classList.remove('active1');
     }
-}
-useEffect(()=>{
-     if(location.pathname =='/' || location.pathname == '/contact'){
+  }
+  useEffect(() => {
+    if (location.pathname == '/' || location.pathname == '/contact') {
       refer.current.classList.remove('navbg');
-       refer.current.classList.add('navbg1');
-       
-     }
-     else if(location.pathname == '/AllEvent' || location.pathname == '/profile'){
+      refer.current.classList.add('navbg1');
+
+    }
+    else if (location.pathname == '/AllEvent' || location.pathname == '/profile') {
       refer.current.classList.remove('navbg1');
       refer.current.classList.add('navbg');
-     }
-     else{
+    }
+    else {
       refer.current.classList.add('navbg');
-     }
-    
-},[location])
-const CustomDropdown = ({ ...props }) => (
-  <Dropdown {...props} >
-    <Link to='/profile'><Dropdown.Item >View Profile</Dropdown.Item></Link>
-    
-    <Link to='/'><Dropdown.Item onClick={()=>{setCurrentName('');setCurrentEmail('') }}>Signout</Dropdown.Item></Link>    
-   
-  </Dropdown>
-);
+    }
+
+  }, [location])
+  const CustomDropdown = ({ ...props }) => (
+    <Dropdown {...props} >
+      <Link to='/profile'><Dropdown.Item >View Profile</Dropdown.Item></Link>
+
+      <Link to='/'><Dropdown.Item onClick={() => { setCurrentName(''); setCurrentEmail('') }}>Signout</Dropdown.Item></Link>
+
+    </Dropdown>
+  );
   return (
     <header>
- 
-        <nav className="navnew" ref={refer}>
+
+      <nav className="d-block navnew navbar navbar-expand-lg" ref={refer}>
         <div class="containernew">
-            <h3 class="logo"><Link href="/">Technova22</Link></h3>
-            <ul>
-            <li><Link to="/" class="current">Home</Link></li>
-              <li><a href="#about">ABout</a></li>
-              <li><Link to="/AllEvent">Events</Link></li>
-              <li><a href="#gallery">Gallery</a></li>
-              <li><Link to="/contact">Contact</Link></li>
-                        {
-                            
-                            (currentName ==='') ?
-                          <li><Link to="/signup" ><button class="btn4 btn btn-outline-primary">Sign Up</button></Link></li>     
-                       :
-                       <CustomDropdown  title={currentName} trigger={['click', 'hover']} />
-                        
-                       
-                          }
-            </ul>
+          <h3 class="logo me-auto"><Link to="/">Technova22</Link></h3>
+          <button class="ms-auto navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="ms-auto collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'><Link to="/" class="ms-auto current">Home</Link></li>
+            <li className='nav-item'><a href="#about">About</a></li>
+            <li className='nav-item'><Link to="/AllEvent">Events</Link></li>
+            <li className='nav-item'><a href="#gallery">Gallery</a></li>
+            <li className='nav-item'><Link to="/contact">Contact</Link></li>
+            {
+
+              (currentName === '') ?
+                <li><Link to="/signup" ><button class="btn4 btn btn-outline-primary text-light">Sign Up</button></Link></li>
+                :
+                <CustomDropdown title={currentName} trigger={['click', 'hover']} />
+
+
+            }
+          </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
 
     </header>
   )
