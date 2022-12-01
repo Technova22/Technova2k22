a8be505ff07f897fba78ef44b31533436ad6299a
abhinavaa01 <agarwalabhinav91@gmail.com>
2022-12-02 00:27:09 +0530

    Gallery section responsive

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,24 +4,61 @@ import "../Gallery.css"
 const Gallery = () => {
   return (
     <>
-    <section id='gallery'>
+    <section id='gallery' className='mb-5'>
     <h3 id='heading_gallery'>GALLERY</h3>
     <div id='line_gallery'></div>
-     <div class="container_gallery">
-  <div class="box_gallery">
-    <img src="images/110.jpeg"/>
+    <hr className='invisible' />
+     <div class="container_gallery row mx-auto">
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0006.webp"/>
     <span>CSS</span>
   </div>
-  <div class="box_gallery">
-    <img src="images/111.jpeg"/>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/DSC_2093.webp"/>
     <span>Image</span>
   </div>
-  <div class="box_gallery">
-    <img src="images/112.jpeg"/>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0018.webp"/>
     <span>Hover</span>
   </div>
-  <div class="box_gallery">
-    <img src="images/113.jpeg"/>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0036.webp"/>
+    <span>Effect</span>
+  </div>
+</div>
+     <div class="container_gallery row">
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0006.webp"/>
+    <span>CSS</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/DSC_2093.webp"/>
+    <span>Image</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0018.webp"/>
+    <span>Hover</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0036.webp"/>
+    <span>Effect</span>
+  </div>
+</div>
+     <div class="container_gallery row">
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0006.webp"/>
+    <span>CSS</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/DSC_2093.webp"/>
+    <span>Image</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0018.webp"/>
+    <span>Hover</span>
+  </div>
+  <div class="box_gallery rounded-5 col-12 col-md-6 col-lg-4 mx-auto py-3">
+    <img className='rounded-5' src="images/webp/_DSC0036.webp"/>
     <span>Effect</span>
   </div>
 </div>
